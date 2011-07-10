4fa00bcab6b6f626afb1dfc46124d12eb26728b0
david <david@david-desktop>
2011-07-09 22:49:06 -0430

    Change-Id: Icccee8b926e9662cec27bbe234383de6672c9589

---
+++ unerg/sql.sql
@@ -0,0 +1,16 @@
+CREATE TABLE usuarios(
+	usuario text,
+	clave text,
+	fecha date,
+	activo int
+);
+
+create table mensajes
+(
+	de text,
+	para text,
+	mensaje text,
+	fecha date,
+	hora time,
+	activo int
+);
